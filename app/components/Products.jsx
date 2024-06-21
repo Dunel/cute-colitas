5cd2514894f5906e70aa2337ef41194cfdc512c7
Daniel Amaya <daniel.amaya18@gmail.com>
2024-06-21 16:04:17 -0400

    products info

--- app/components/Products.jsx
+++ app/components/Products.jsx
@@ -3,21 +3,21 @@ import React from "react";
 const productList = [
   {
     name: "Scrunchie",
-    price: "$5",
+    price: "$2",
     description: "Scrunchie decorada con flores.",
     image: "https://xxlandco.com/cdn/shop/files/22_everyonesfavmini_720x.jpg",
   },
   {
     name: "Moñeras",
-    price: "$0.5",
+    price: "$1",
     description: "Moñeras para el cabello con flores.",
     image: "/images/moñeras.png",
   },
   {
     name: "Lazo",
     price: "$4",
-    description: "Lazo para el cabello de satén XXL de gran tamaño, completo con una pinza de cocodrilo en la parte posterior para permanecer en su sitio durante todo el día.",
-    image: "https://xxlandco.com/cdn/shop/files/xxlsatinleyahbow_540x.jpg",
+    description: "Lazo para el cabello de satén de gran tamaño.",
+    image: "https://xxlandco.com/cdn/shop/files/xxlsatinleyahbow_360x.jpg",
   },
   {
     name: "Antifaz",
@@ -27,21 +27,25 @@ const productList = [
   },
 ];
 
-
 const Products = ({ products = productList }) => {
   return (
-    <section id="products" className="section py-8">
-      <h2 className="text-2xl md:text-3xl mb-8 text-center">Nuestros Productos</h2>
+    <section id="products" className="section bg-white w-full text-[#352b4d] text-justify leading-relaxed text-2xl py-8">
+      <h2 className="text-5xl font-bold mb-4 text-center">Nuestros Productos</h2>
       <div className="product-grid flex flex-wrap justify-center gap-4">
         {products.map((product, index) => (
           <div
             key={index}
-            className="product-card border p-4 m-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 bg-white shadow-lg rounded-lg"
+            className="product-card border p-4 m-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 bg-white shadow-lg rounded-lg flex flex-col justify-between h-[480px]"
           >
-            <img src={product.image} alt={product.name} className="w-full h-48 object-cover rounded-t-lg mb-4" />
-            <h3 className="text-xl mb-2">{product.name}</h3>
-            <p>{product.description}</p>
-            <p className="text-lg font-bold mt-2">{product.price}</p>
+            <div className="product-info flex flex-col items-center text-center">
+            <img src={product.image} alt={product.name} className="w-[200px] h-[200px] rounded-t-lg mb-4" />
+              <h3 className="text-xl mb-2">{product.name}</h3>
+              <p className="flex-grow overflow-hidden text-ellipsis">{product.description}</p>
+              <p className="text-lg font-bold mt-2">{product.price}</p>
+              <a href="https://instagram.com/cutecolitas" target="_blank" rel="noopener noreferrer">
+                <button className="mt-4 bg-[#352b4d] text-white py-2 px-4 rounded-full">Comprar</button>
+              </a>
+            </div>
           </div>
         ))}
       </div>
@@ -50,4 +54,3 @@ const Products = ({ products = productList }) => {
 };
 
 export default Products;
-
