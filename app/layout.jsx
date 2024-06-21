acc3b76fcc4bc5228bbfd92fe167aa99fd6e9984
Edily Mora <edilyrosa@gmail.com>
2024-06-21 15:56:15 -0400

    test 5

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,7 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="es" class="scroll-smooth focus:scroll-smooth">
+    <html lang="es" className="scroll-smooth focus:scroll-smooth">
       <Head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
